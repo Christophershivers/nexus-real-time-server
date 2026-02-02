2ee23e59c1edf61db85005619cdec38c16431926
Christopher Shivers <chrisshivers55@gmail.com>
2026-02-02 16:15:23 -0600

    added logging

--- lib/nexus_realtime_server/main.ex
+++ lib/nexus_realtime_server/main.ex
@@ -3,7 +3,7 @@ defmodule NexusRealtimeServer.Main do
   alias NexusRealtimeServer.FetchQueries
 
   def main(payload) do
-
+    IO.inspect(payload, lebel: "payload data")
     queries = InMemoryTopicParser.parse(payload)
     IO.inspect(queries, label: "In Main module")
     FetchQueries.run(queries)
